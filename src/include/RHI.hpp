11fcb9fb9190e3d36570d6c4bd240457215259cd
Andromeda08 <vkovacsbalazs26@gmail.com>
2025-03-29 22:08:14 +0100

    RHI.hpp header

---
+++ src/include/RHI.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "RHI/Definitions.hpp"
+#include "RHI/DynamicRHI.hpp"
+#include "RHI/Frame.hpp"
+#include "RHI/RHIBuffer.hpp"
+#include "RHI/RHICommandList.hpp"
+#include "RHI/RHICommandQueue.hpp"
+#include "RHI/RHIFramebuffer.hpp"
+#include "RHI/RHIPipeline.hpp"
+#include "RHI/RHIRenderPass.hpp"
+#include "RHI/RHISwapchain.hpp"
+#include "RHI/RHITexture.hpp"
+#include "RHI/RHIWindow.hpp"
+
+rhi_BEGIN_NAMESPACE;
+
+std::unique_ptr<DynamicRHI> createRHI(const RHICreateInfo& rhiCreateInfo);
+
+rhi_END_NAMESPACE;
